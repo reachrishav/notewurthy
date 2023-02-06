64b19d1d7e5661addcd5ea6eadacbf5879e0da87
rhitabratakarar <kararrhitabrata@gmail.com>
2023-02-06 16:16:32 +0530

    Dark theme persists on reload

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,20 +1,25 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 
 const ThemeContext = React.createContext()
 const ThemeUpdateContext = React.createContext()
 
-export function useTheme(){
+export function useTheme() {
     return useContext(ThemeContext)
 }
 
-export function useThemeUpdate(){
+export function useThemeUpdate() {
     return useContext(ThemeUpdateContext)
 }
 
-export function ThemeProvider({children}){
-    const [darkTheme, setDarkTheme] = useState(false)
+export function ThemeProvider({ children }) {
+    const DARKTHEME = 'darkTheme'
+    const [darkTheme, setDarkTheme] = useState(localStorage.getItem(DARKTHEME) === "true")
 
-    function toggleTheme(){
+    useEffect(() => {
+        localStorage.setItem(DARKTHEME, darkTheme)
+    }, [darkTheme])
+
+    function toggleTheme() {
         setDarkTheme(prevTheme => !prevTheme)
     }
 
