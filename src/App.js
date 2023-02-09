7e2fddb143ef2696aadca9c862ba78115deda383
rhitabratakarar <kararrhitabrata@gmail.com>
2023-02-09 20:18:53 +0530

    Dark Mode Animation

--- src/App.js
+++ src/App.js
@@ -4,42 +4,47 @@ import { ThemeProvider } from "./ThemeContext"
 import { Routes, Route } from "react-router-dom"
 import BlogPost from "./components/BlogPost"
 import NotFound from "./components/NotFound"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
-  const [blogs, setBlogs] = useState([
-    {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    setBlogs([
+      {
         id: 1,
         title: 'Designing in the Build',
-        description:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.', 
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.',
         datePublished: 'FEB 02, 2023'
-    },
-    {
+      },
+      {
         id: 2,
         title: 'Second Blog',
-        description:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.', 
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.',
         datePublished: 'FEB 02, 2023'
-    },
-    {
+      },
+      {
         id: 3,
         title: 'Third Blog',
-        description:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.', 
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.',
         datePublished: 'FEB 02, 2023'
-    },
-    {
+      },
+      {
         id: 4,
         title: 'Fourth Blog',
-        description:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.', 
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, iure.',
         datePublished: 'FEB 02, 2023'
-    }
-])
+      }
+    ])
+  }, [blogs])
+  
   return (
     <ThemeProvider>
-        <Routes>
-          <Route path="/" element={<Content blogs={blogs}/>} />
-          <Route path="/blog/:id" element={<BlogPost blogs={blogs}/>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Content blogs={blogs} />} />
+        <Route path="/blog/:id" element={<BlogPost blogs={blogs} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </ThemeProvider>
   )
 }
