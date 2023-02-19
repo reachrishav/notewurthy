99123494fc4cdf71944b4ce4105c16f244dcaa00
Rishav <reachrishav@gmail.com>
2023-02-19 22:23:21 +0530

    add xcircle in blog post component

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,27 +1,29 @@
-import { useTheme } from "../ThemeContext"
-import NotFound from "./NotFound"
+import { useTheme } from '../ThemeContext'
+import NotFound from './NotFound'
 
 const BlogPost = ({ blogs, id }) => {
-  const darkTheme = useTheme()
+	const darkTheme = useTheme()
 
-  const themeStyles = {
-    backgroundColor: darkTheme ? "#23272f" : "#fff",
-    color: darkTheme ? "#fff" : "#23272f",
-    transition: "background-color .5s ease"
-  }
+	const themeStyles = {
+		backgroundColor: darkTheme ? '#23272f' : '#fff',
+		color: darkTheme ? '#fff' : '#23272f',
+		transition: 'background-color .5s ease',
+	}
 
-  const filteredBlog = blogs.find((blog) => blog.id === parseInt(id))
-  if(!filteredBlog){
-    return <NotFound/>
-  }
-  return (
-    <div style={themeStyles}>
-
-      <h1>{filteredBlog.title}</h1>
-      <br />
-      <p className="light-font-weight">{filteredBlog.description}</p>
-    </div>
-  )
+	const filteredBlog = blogs.find((blog) => blog.id === parseInt(id))
+	if (!filteredBlog) {
+		return <NotFound />
+	}
+	return (
+		<div style={themeStyles}>
+			<div className="blog-post-header flex">
+				<h1>{filteredBlog.title}</h1>
+				<i className="fa-duotone fa-circle-xmark"></i>
+			</div>
+			<br />
+			<p className="light-font-weight">{filteredBlog.description}</p>
+		</div>
+	)
 }
 
 export default BlogPost
